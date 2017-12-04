c549720311922388161b7ce5befba028be003b3f
Ilya Burakov <speedwatson@gmail.com>
2017-12-04 19:53:34 +0300

    Rewrite assemble.bat

--- assemble.bat
+++ assemble.bat
@@ -1,36 +1,23 @@
 @echo off
  
-set Portable.RemoteTasks.Manager.Password=
-set Portable.RemoteTasks.Manager.Login=
- 
 setlocal
-set asm=..\asm_listing.als
-set bin_name=out.ptptb
-set arh=..\yafn.arch
- 
-IF NOT "%1"=="" (
-    set asm=%1
+IF NOT %1.==. (
+	set asm_file=%1
+	set local_bin_name=out.ptptb
+
+	set guid=
+	FOR /F %%I IN ('..\RemoteTasks\rt -id -v -w -s Assemble definitionFile goose.arch archName fib.asm asmListing %asm_file%') DO  set guid=%%I
+	 
+	IF NOT %guid%.==. (
+		..\RemoteTasks\rt -g %guid%
+		..\RemoteTasks\rt -g %guid% -r out.ptptb -o ..\GooseVM\%local_bin_name%
+	) ELSE (
+		echo Could not retrieve task GUID from RemoteTasks!
+		echo Last task:
+		..\RemoteTasks\rt -l 0
+	)
+) ELSE (
+	echo No file to assemble was specified!
+	echo Usage: assemble.bat ^<filename^>
 )
- 
-IF NOT "%2"=="" (
-    set bin_name=%2
-)
- 
-IF NOT "%3"=="" (
-    set arh=%3
-)
- 
- 
- 
-set guid=
-FOR /F %%I IN ('.\Portable.RemoteTasks.Manager.exe -id -v -w -s AssembleDebug definitionFile %arh% archName yafn asmListing %asm% sourcesDir .') DO  set guid=%%I
- 
-Portable.RemoteTasks.Manager.exe -g %guid%
-Portable.RemoteTasks.Manager.exe -g %guid% -r out.ptptb -o %bin_name%
- 
- 
-copy /b/v/y %bin_name% ..\..\..\Dev\yafn\yafn\content
-copy /b/v/y %asm% ..\..\..\Dev\yafn\yafn\content
-copy /b/v/y %arh% ..\..\..\Dev\yafn\yafn\content
- 
 endlocal
