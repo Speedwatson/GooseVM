92a5488c5561f86af443aa74a2f960e86e2fe080
Ilya Burakov <speedwatson@gmail.com>
2017-12-08 23:55:21 +0300

    ExecutableFile integration

--- Interpreter/Interpreter.cpp
+++ Interpreter/Interpreter.cpp
@@ -1,19 +1,34 @@
 #include "stdafx.h"
+#include "globals.hpp"
 #include "Arguments.hpp"
 #include "Exceptions.hpp"
+#include "ExecutableFile.hpp"
 
 int main(int argc, char* argv[]) {
-	std::vector<std::string> argvstr(argc - 1);
-	for (int i = 1; i < argc; ++i) argvstr[i - 1] = argv[i];
-	
-	try {
-		Arguments args(argvstr);
+	std::vector<std::string> argvec(argc - 1);
+	for (int i = 1; i < argc; ++i) argvec[i - 1] = argv[i];
 
-		// create Machine and run it...
-	}
-	catch (UnknownArgError& e) {
-		std::cerr << e.what();
-	}
+	try {
+		Arguments args(argvec);
+
+		if (args.get(args.argHelp)) {
+			std::cout << HELP_STRING << '\n';
+			return EXIT_SUCCESS;
+		}
+
+		const ExecutableFile execfile(argvec[0]);
 		
-	return 0;
+		std::cout << "Sections count: " << execfile.tablesHeader.sectionsCount << "\nBlobs count: " << execfile.tablesHeader.blobsCount << "\n";
+
+		return EXIT_SUCCESS;
+	}
+	catch (Error& e) {
+		std::cerr << e.what();
+		return EXIT_FAILURE;
+	}
+	catch (std::bad_alloc& e) {
+		std::cerr << "Could't allocate memory: " << e.what() << ".\n";
+		return EXIT_FAILURE;
+	}
+
 }
--- Interpreter/stdafx.h
+++ Interpreter/stdafx.h
@@ -1,7 +1,9 @@
 #include <iostream>
+#include <fstream>
 #include <string>
 #include <vector>
 #include <cstdint>
 #include <algorithm>
 #include <array>
 #include <exception>
+#include <memory>
